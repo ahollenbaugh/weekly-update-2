4251f5179ad5a61fa5b72c5fdf54454fe6997386
ahollenbaugh <56135023+ahollenbaugh@users.noreply.github.com>
2023-09-26 21:23:15 -0700

    variable instead of string literal, change 9 to 7

--- weekly-update-2.py
+++ weekly-update-2.py
@@ -3,8 +3,9 @@ content = f.read()
 f.close()
 
 vowels = 'aeiouy'
+replace_with = "7"
 
 for vowel in vowels:
-    content = content.replace(vowel, "9").replace(vowel.upper(), "9")
+    content = content.replace(vowel, replace_with).replace(vowel.upper(), replace_with)
 
 print(content)
